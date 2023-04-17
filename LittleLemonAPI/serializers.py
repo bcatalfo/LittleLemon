2783614848c5e68192e14b07d94a3433ba2dfccf
Benjamin Catalfo <bcatalfo@gmail.com>
2023-04-17 16:45:54 -0400

    Add categories to menu items.

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers
-from .models import MenuItem
+from .models import MenuItem, Category
 from decimal import Decimal
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "slug", "title"]
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source="inventory")
     price_after_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+    category = CategorySerializer()
 
     class Meta:
         model = MenuItem
-        fields = ["id", "title", "price_after_tax", "stock"]
+        fields = ["id", "title", "price_after_tax", "stock", "category"]
 
     def calculate_tax(self, product: MenuItem):
         return product.price * Decimal(1.1)
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,13 +1,19 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from django.shortcuts import get_object_or_404
 
 
-class MenuItemsView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+@api_view()
+def menu_items(request):
+    items = MenuItem.objects.select_related("category").all()
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response(serialized_item.data)
 
 
-class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+@api_view()
+def single_item(request, id):
+    item = get_object_or_404(MenuItem, pk=id)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
