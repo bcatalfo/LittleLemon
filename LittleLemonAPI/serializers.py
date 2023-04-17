571c0f54e582062a4f2558a007b0fb66067089c5
Benjamin Catalfo <bcatalfo@gmail.com>
2023-04-17 13:39:37 -0400

    Rename 'inventory' to 'stock'.

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -3,6 +3,8 @@ from .models import MenuItem
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    stock = serializers.IntegerField(source="inventory")
+
     class Meta:
         model = MenuItem
-        fields = ["id", "title", "price", "inventory"]
+        fields = ["id", "title", "price", "stock"]
