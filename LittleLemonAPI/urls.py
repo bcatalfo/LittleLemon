c4006b9c42d4927d5f254146c13edf6480534a03
Benjamin Catalfo <bcatalfo@gmail.com>
2023-04-17 16:56:33 -0400

    Add category details to API.

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("menu-items/", views.menu_items),
     path("menu-items/<int:id>", views.single_item),
+    path("category/<int:pk>", views.category_detail, name="category-detail"),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
 from django.shortcuts import get_object_or_404
 
 
@@ -17,3 +17,10 @@ def single_item(request, id):
     item = get_object_or_404(MenuItem, pk=id)
     serialized_item = MenuItemSerializer(item)
     return Response(serialized_item.data)
+
+
+@api_view()
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    serialized_category = CategorySerializer(category)
+    return Response(serialized_category.data)
