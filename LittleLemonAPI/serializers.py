12d1c18ce85a52e039cdf996adedf145dafe1502
Benjamin Catalfo <bcatalfo@gmail.com>
2023-04-17 13:49:23 -0400

    Calculate price after tax.

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import MenuItem
+from decimal import Decimal
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source="inventory")
+    price_after_tax = serializers.SerializerMethodField(method_name="calculate_tax")
 
     class Meta:
         model = MenuItem
-        fields = ["id", "title", "price", "stock"]
+        fields = ["id", "title", "price_after_tax", "stock"]
+
+    def calculate_tax(self, product: MenuItem):
+        return product.price * Decimal(1.1)
