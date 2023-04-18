b74adfd02893e280412494e54fb717d50f7269e8
Benjamin Catalfo <bcatalfo@gmail.com>
2023-04-18 10:26:20 -0400

    Add pagination.

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -4,6 +4,7 @@ from .models import MenuItem, Category
 from .serializers import MenuItemSerializer, CategorySerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from django.core.paginator import Paginator, EmptyPage
 
 
 @api_view(["GET", "POST"])
@@ -14,6 +15,8 @@ def menu_items(request):
         to_price = request.query_params.get("to_price")
         search = request.query_params.get("search")
         ordering = request.query_params.get("ordering")
+        perpage = request.query_params.get("perpage", default=2)
+        page = request.query_params.get("page", default=1)
         if category_name:
             items = items.filter(category__title=category_name)
         if to_price:
@@ -23,6 +26,11 @@ def menu_items(request):
         if ordering:
             ordering_fields = ordering.split(",")
             items = items.order_by(*ordering_fields)
+        paginator = Paginator(items, per_page=perpage)
+        try:
+            items = paginator.page(number=page)
+        except EmptyPage:
+            items = []
         serialized_item = MenuItemSerializer(items, many=True)
         return Response(serialized_item.data)
     if request.method == "POST":
