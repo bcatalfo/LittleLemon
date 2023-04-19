5a9894dfb919428cb29c5fa98ef799602c299ae2
Benjamin Catalfo <bcatalfo@gmail.com>
2023-04-19 13:23:38 -0400

    Add user account management.

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,8 +1,9 @@
 from django.core.paginator import EmptyPage, Paginator
+from django.contrib.auth.models import User, Group
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes, throttle_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
@@ -83,3 +84,18 @@ def throttle_check(request):
 @throttle_classes([TenCallsPerMinute])
 def throttle_check_auth(request):
     return Response({"message": "message for the logged in users only"})
+
+
+@api_view(["POST"])
+@permission_classes([IsAdminUser])
+def managers(request):
+    username = request.data["username"]
+    if username:
+        user = get_object_or_404(User, username=username)
+        managers = Group.objects.get(name="Manager")
+        if request.method == "POST":
+            managers.user_set.add(user)
+        elif request.method == "DELETE":
+            managers.user_set.remove(user)
+        return Response({"message": "ok"})
+    return Response({"message": "error"}, status.HTTP_400_BAD_REQUEST)
