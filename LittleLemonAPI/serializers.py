536317789a92e4afae7bf9388f1ea955ad02a2a1
Benjamin Catalfo <bcatalfo@gmail.com>
2023-04-25 20:13:39 -0400

    Add id and remove groups from UserSerializer.

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "email", "groups"]
+        fields = ["id", "username", "first_name", "last_name", "email"]
 
 
 class CategorySerializer(serializers.ModelSerializer):
